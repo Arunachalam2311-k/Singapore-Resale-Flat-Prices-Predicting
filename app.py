363c82433061f5297abdf70dca2cf6c39f4ef589
Arunachalam2311-k <157885511+Arunachalam2311-k@users.noreply.github.com>
2024-08-20 09:32:41 +0530

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
+# Import necessary libraries
 import streamlit as st
 import pandas as pd
-import numpy as np
-import pickle
 from sklearn.ensemble import RandomForestRegressor
+import os
+import pickle
 
-
-# Set page configuration to use wide layout
+# Set the page configuration to have a wide layout
 st.set_page_config(layout='wide')
 
-st.markdown("<h1 style='text-align: center; color: red;'>Singapore Flat Resale Price Prediction</h1>", unsafe_allow_html=True)
+# Add a title to the Streamlit app
+st.markdown("<h1 style='text-align: center; color: blue;'>Singapore Flat Resale Price Prediction</h1>", unsafe_allow_html=True)
 
-# Load the dataset
-df = pd.read_csv(r'D:\singapore\final.csv')
+# Create three tabs: HOME, PREDICTION, PREDICTION AND CONCLUSION
+tab1, tab2, tab3 = st.tabs(['HOME', 'PREDICTION', 'PREDICTION AND CONCLUSION'])
 
-# Create tabs for navigation
-tab1, tab2,tab3 = st.tabs(['HOME','PREDICTION','CONCLUSION'])
-
-
+# HOME tab content
 with tab1:
     col1, col2 = st.columns(2, gap='large')
 
@@ -29,33 +27,30 @@ with tab1:
 
     with col2:
         st.write('## Tools and Technologies used')
-        st.write('Python, Pandas, Numpy, Matplotlib, Seaborn, Plotly, Streamlit, Sklearn')
+        st.write(' Python, Pandas, numpy, matplotlib, seaborn, Plotly, Streamlit, sklearn')
         st.write('## :red[ML Model]')
         st.write('* The ML model used in this project is :blue[Random Forest Regressor].')
         st.write('* Comparing other regressors, Random Forest Regressor had a high :red[R-squared score], which means it has performed best.')
 
+# PREDICTION tab content
 with tab2:
-    option = st.radio('**Select your option**', ('Processed Data', 'Prediction Tab'), horizontal=True)
+    col3, col4, col5 = st.columns(3, gap="small")
 
-    if option == "Processed Data":
-        st.header('Processed Final Data')
-        st.write(df)
+    # Example: Adding prediction input fields
+    with col3:
+        town_mapping = {
+            'ANG MO KIO': 1, 'BEDOK': 2, 'BISHAN': 3, 'BUKIT BATOK': 4, 'BUKIT MERAH': 5, 'BUKIT TIMAH': 6,
+            'CENTRAL AREA': 7, 'CHOA CHU KANG': 8, 'CLEMENTI': 9, 'GEYLANG': 10, 'HOUGANG': 11,
+            'JURONG EAST': 12, 'JURONG WEST': 13, 'KALLANG/WHAMPOA': 14, 'MARINE PARADE': 15, 'QUEENSTOWN': 16,
+            'SENGKANG': 17, 'SERANGOON': 18, 'TAMPINES': 19, 'TOA PAYOH': 20, 'WOODLANDS': 21, 'YISHUN': 22,
+            'LIM CHU KANG': 23, 'SEMBAWANG': 24, 'BUKIT PANJANG': 25, 'PASIR RIS': 26, 'PUNGGOL': 27
+        }
 
-    elif option == "Prediction Tab":
-        col3, col4, col5 = st.columns(3, gap="small")
-
-
-        with col3:
-            town_mapping ={'ANG MO KIO': 1,'BEDOK': 2,'BISHAN': 3,'BUKIT BATOK': 4,'BUKIT MERAH': 5,
-                                'BUKIT TIMAH': 6,'CENTRAL AREA': 7,'CHOA CHU KANG': 8,'CLEMENTI': 9,'GEYLANG': 10,
-                                'HOUGANG': 11,'JURONG EAST': 12,'JURONG WEST': 13,'KALLANG/WHAMPOA': 14,'MARINE PARADE': 15,'QUEENSTOWN': 16,
-                                'SENGKANG': 17,'SERANGOON': 18,'TAMPINES': 19,'TOA PAYOH': 20,'WOODLANDS': 21,'YISHUN': 22,'LIM CHU KANG': 23,
-                                'SEMBAWANG': 24,'BUKIT PANJANG': 25,'PASIR RIS': 26,'PUNGGOL': 27}
-            town_key = st.selectbox('**Select a town**',list(town_mapping.keys()))
-            town = town_mapping[town_key]
+        town_key = st.selectbox('**Select a town**', list(town_mapping.keys()))
+        town = town_mapping[town_key] 
 
             # street dictionary mapping to numbers
-            streets = {'ANG MO KIO AVE 1': 1, 'ANG MO KIO AVE 3': 2, 'ANG MO KIO AVE 4': 3, 'ANG MO KIO AVE 10': 4,
+        streets = {'ANG MO KIO AVE 1': 1, 'ANG MO KIO AVE 3': 2, 'ANG MO KIO AVE 4': 3, 'ANG MO KIO AVE 10': 4,
                 'ANG MO KIO AVE 5': 5,
                 'ANG MO KIO AVE 8': 6, 'ANG MO KIO AVE 6': 7, 'ANG MO KIO AVE 9': 8, 'ANG MO KIO AVE 2': 9,
                 'BEDOK RESERVOIR RD': 10, 'BEDOK NTH ST 3': 11, 'BEDOK STH RD': 12, 'NEW UPP CHANGI RD': 13,
@@ -236,120 +231,120 @@ with tab2:
                 'ANG MO KIO ST 51': 580,
                 'BT BATOK EAST AVE 6': 581, 'BT BATOK WEST AVE 9': 582}
            
-            street = st.selectbox("**Select Street**", list(streets.keys()))
-            st_value = streets[street]
+        street = st.selectbox("**Select Street**", list(streets.keys()))
+        st_value = streets[street]
 
         
-            # get block details
-            block = st.text_input('**Enter the block number (eg.201A)**', value=254)
-            # Define a mapping for letters to decimal values
-            letter_mapping = {chr(ord('A') + i): f'.{i + 1}' for i in range(26)}
-            block_decimal = float(''.join(letter_mapping.get(c, c) for c in block))
+        # get block details
+        block = st.text_input('**Enter the block number (eg.201A)**', value=254)
+        # Define a mapping for letters to decimal values
+        letter_mapping = {chr(ord('A') + i): f'.{i + 1}' for i in range(26)}
+        block_decimal = float(''.join(letter_mapping.get(c, c) for c in block))
 
-            # Define a mapping of flat_type to numbers
-            category_mapping = {
-            '1 ROOM': 1,
-            '2 ROOM': 2,
-            '3 ROOM': 3,
-            '4 ROOM': 4,
-            '5 ROOM': 5,
-            'EXECUTIVE': 6,
-            'MULTI GENERATION': 7
-            }
+        # Define a mapping of flat_type to numbers
+        category_mapping = {
+        '1 ROOM': 1,
+        '2 ROOM': 2,
+        '3 ROOM': 3,
+        '4 ROOM': 4,
+        '5 ROOM': 5,
+        'EXECUTIVE': 6,
+        'MULTI GENERATION': 7
+        }
 
-            flat_type = st.selectbox('**Select Flat Type**', list(category_mapping.keys()))
-            flat_type_value = category_mapping[flat_type]
+        flat_type = st.selectbox('**Select Flat Type**', list(category_mapping.keys()))
+        flat_type_value = category_mapping[flat_type]
 
 
-    
-        with col4:
-            # Flat Model
-            flat_model_mapping = {
-                'IMPROVED': 1, 'NEW GENERATION': 2, 'MODEL A': 3, 'STANDARD': 4,
-                'SIMPLIFIED': 5, 'MODEL A-MAISONETTE': 6, 'APARTMENT': 7, 'MAISONETTE': 8,
-                'TERRACE': 9, '2-ROOM': 10, 'IMPROVED-MAISONETTE': 11, 'MULTI GENERATION': 12,
-                'PREMIUM APARTMENT': 13, 'Improved': 14, 'New Generation': 15, 'Model A': 16,
-                'Standard': 17, 'Apartment': 18, 'Simplified': 19, 'Model A-Maisonette': 20,
-                'Maisonette': 21, 'Multi Generation': 22, 'Adjoined flat': 23, 'Premium Apartment': 24,
-                'Terrace': 25, 'Improved-Maisonette': 26, 'Premium Maisonette': 27, '2-room': 28,
-                'Model A2': 29, 'DBSS': 30, 'Type S1': 31, 'Type S2': 32, 'Premium Apartment Loft': 33, '3Gen': 34
-            }
 
-            flat_model = st.selectbox('**Select Flat Model**', list(flat_model_mapping.keys()))
-            flat_type_value = flat_model_mapping[flat_model]
+    with col4:
+        # Flat Model
+        flat_model_mapping = {
+            'IMPROVED': 1, 'NEW GENERATION': 2, 'MODEL A': 3, 'STANDARD': 4,
+            'SIMPLIFIED': 5, 'MODEL A-MAISONETTE': 6, 'APARTMENT': 7, 'MAISONETTE': 8,
+            'TERRACE': 9, '2-ROOM': 10, 'IMPROVED-MAISONETTE': 11, 'MULTI GENERATION': 12,
+            'PREMIUM APARTMENT': 13, 'Improved': 14, 'New Generation': 15, 'Model A': 16,
+            'Standard': 17, 'Apartment': 18, 'Simplified': 19, 'Model A-Maisonette': 20,
+            'Maisonette': 21, 'Multi Generation': 22, 'Adjoined flat': 23, 'Premium Apartment': 24,
+            'Terrace': 25, 'Improved-Maisonette': 26, 'Premium Maisonette': 27, '2-room': 28,
+            'Model A2': 29, 'DBSS': 30, 'Type S1': 31, 'Type S2': 32, 'Premium Apartment Loft': 33, '3Gen': 34
+        }
 
-            # Input for area in sq.m
-            floor_area = st.number_input("**Enter the area (in sq.m)**", value=35.0)
+        flat_model = st.selectbox('**Select Flat Model**', list(flat_model_mapping.keys()))
+        flat_type_value = flat_model_mapping[flat_model]
 
-            # Storey range input
-            storey_lower = st.number_input('**Enter the lower bound of the storey range**', value=4, min_value=0)
-            storey_upper = st.number_input('**Enter the upper bound of the storey range**', value=6, min_value=storey_lower)
+        # Input for area in sq.m
+        floor_area = st.number_input("**Enter the area (in sq.m)**", value=35.0)
 
-        with col5:
+        # Storey range input
+        storey_lower = st.number_input('**Enter the lower bound of the storey range**', value=4, min_value=0)
+        storey_upper = st.number_input('**Enter the upper bound of the storey range**', value=6, min_value=storey_lower)
 
-            # Lease commence year
-            lease_commence_year = st.number_input("**Enter the lease commence year**", value=1990)
+    with col5:
 
-            resale_year = st.number_input('**Enter the resale year**', value=2016, min_value=lease_commence_year)
-            resale_month = st.number_input('**Enter the resale month**', value=12, min_value=1, max_value=12)
+        # Lease commence year
+        lease_commence_year = st.number_input("**Enter the lease commence year**", value=1990)
 
-            features = {
-                'town': town,
-                'flat_type': flat_type_value,
-                'block': block_decimal,
-                'street_name': st_value,
-                'floor_area_sqm': floor_area,
-                'flat_model': flat_type_value,
-                'lease_commence_date': lease_commence_year,
-                'resale_year': resale_year,
-                'resale_month': resale_month,
-                'storey_lower_bound': storey_lower,
-                'storey_upper_bound': storey_upper,
-            }
+        resale_year = st.number_input('**Enter the resale year**', value=2016, min_value=lease_commence_year)
+        resale_month = st.number_input('**Enter the resale month**', value=12, min_value=1, max_value=12)
 
-            features_df = pd.DataFrame(features, index=[1])
+        features = {
+            'town': town,
+            'flat_type': flat_type_value,
+            'block': block_decimal,
+            'street_name': st_value,
+            'floor_area_sqm': floor_area,
+            'flat_model': flat_type_value,
+            'lease_commence_date': lease_commence_year,
+            'resale_year': resale_year,
+            'resale_month': resale_month,
+            'storey_lower_bound': storey_lower,
+            'storey_upper_bound': storey_upper,
+        }
+
+        features_df = pd.DataFrame(features, index=[1])
+        
+
+        # predict the resale price
+        if st.button('Predict'):
+
+            def load_data():
+                # Load data
+                data = pd.read_csv(r'D:\singapore\final.csv')
+                sample_df = data.sample(n=5000, random_state=42)
+                return sample_df
+
+            c_df = load_data()
+
+            def train_model(data):
+                # Train model here
+                x = data.drop(['resale_price', 'Unnamed: 0'], axis=1)
+                y = data['resale_price']
+                trained_model = RandomForestRegressor(random_state=42)
+                trained_model.fit(x, y)
+                return trained_model
             
+            rf_regressor = train_model(c_df)
 
-            # predict the resale price
-            if st.button('Predict'):
+            # Check and handle missing columns
+            missing_cols = set(rf_regressor.feature_names_in_) - set(features_df.columns)
+            if missing_cols:
+                st.error(f"Missing columns in input data: {missing_cols}")
+                for col in missing_cols:
+                    # Add missing columns with default values (e.g., 0 or mean)
+                    features_df[col] = 0  # You may choose a different default value
 
-                def load_data():
-                    # Load data
-                    data = pd.read_csv(r'D:\singapore\final.csv')
-                    sample_df = data.sample(n=5000, random_state=42)
-                    return sample_df
+            # Ensure the feature columns are in the same order as during training
+            features_df = features_df[rf_regressor.feature_names_in_]
 
-                c_df = load_data()
+            # Use the trained model to make predictions
+            prediction = rf_regressor.predict(features_df)
 
-                def train_model(data):
-                    # Train model here
-                    x = data.drop(['resale_price', 'Unnamed: 0'], axis=1)
-                    y = data['resale_price']
-                    trained_model = RandomForestRegressor(random_state=42)
-                    trained_model.fit(x, y)
-                    return trained_model
-                
-                rf_regressor = train_model(c_df)
+            predicted_price = str(prediction)[1:-1]
 
-                # Check and handle missing columns
-                missing_cols = set(rf_regressor.feature_names_in_) - set(features_df.columns)
-                if missing_cols:
-                    st.error(f"Missing columns in input data: {missing_cols}")
-                    for col in missing_cols:
-                        # Add missing columns with default values (e.g., 0 or mean)
-                        features_df[col] = 0  # You may choose a different default value
+            # Display the prediction
+            st.success(f"Resale Price: :red[S$] :green[{predicted_price}]")
 
-                # Ensure the feature columns are in the same order as during training
-                features_df = features_df[rf_regressor.feature_names_in_]
-
-                # Use the trained model to make predictions
-                prediction = rf_regressor.predict(features_df)
-
-                predicted_price = str(prediction)[1:-1]
-
-                # Display the prediction
-                st.success(f"Resale Price: :red[S$] :green[{predicted_price}]")
-    
 with tab3:
 
             st.markdown('### :violet[Some Insights...]')
@@ -359,5 +354,7 @@ with tab3:
             st.write('* Older flats may have lower resale values, especially if they require renovations or if the lease is running out.')
             st.write('* :red[Population growth, changes in demographics], and :red[urban development plans] can influence the demand for housing in certain areas.')
             st.write('* Overall, this project will be useful in :red[predicting the resale prices of flats and understanding the key factors] that can enhance their value.')
+
+
 
 
